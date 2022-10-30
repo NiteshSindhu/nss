1d4a401bf6fbe550858f19297fba74220a7859bb
NITESH SINDHU <niteshsindhu1435@gmail.com>
2022-10-30 23:56:14 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import AllRoutes from './Routes/AllRoutes';
+
+
+
+function App() {
+
+
+  return (
+
+    <div className="App">
+     
+    
+        <AllRoutes/>
+    </div>
+
+
+
+  );
+}
+
+export default App;
